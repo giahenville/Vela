2fe5c4c7e425df365eb6f63b954387b619b6ebac
Gia Henville <140769584+giabui@users.noreply.github.com>
2025-07-11 21:39:14 -0400

    feat: Create basic landing page

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const apiRoutes = require('./src/routes/api');
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from /public
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 // Middleware to parse JSON
 app.use(express.json());
